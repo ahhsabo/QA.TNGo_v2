b67a068b09124b2b1a09cde20bbd8d7a6de7195d
DoQuangAnh <anhdk2000@gmail.com>
2023-08-21 17:35:49 +0700

    Initial commit

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QA.SportStore.Models;
+
+namespace QA.SportStore.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Login> Login { get; set; }
+    }
+}
---
+++ Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QA.SportStore.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string? UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
