9cfa2c5b1e382e3cf939d524524fa1afdb6b8497
DoQuangAnh <anhdk2000@gmail.com>
2023-09-27 17:29:04 +0700

    commit
    Summernote

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -4,18 +4,18 @@ using QA.TNGo_v2.Models;
 
 namespace QA.TNGo_v2.Controllers
 {
-    public class ModalController : Controller
+    public class GalleryController : Controller
     {
         private readonly ApplicationContext _context;
 
-        public ModalController(ApplicationContext context)
+        public GalleryController(ApplicationContext context)
         {
             _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return PartialView("_Gallery"); ;
         }
     }
 }
